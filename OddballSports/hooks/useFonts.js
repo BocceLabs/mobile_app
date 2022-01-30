01fc5f0bd11f6069d2f10828a953af839684221c
David Hoffman <david.r.hoffman@ieee.org>
2022-01-30 13:18:37 -0600

    starting fresh after course

--- OddballSports/hooks/useFonts.js
+++
@@ -1,7 +0,0 @@
-import * as Font from "expo-font";
-
-export default useFonts = async () => {
-    await Font.loadAsync({
-        "Orbitron-Regular": require("./assets/fonts/Orbitron-Regular.ttf"),
-    });
-};
